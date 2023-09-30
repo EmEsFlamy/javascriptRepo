2ef4b25d77458db0f80509de34a70f40a43bea9a
unknown <Sebastian.Rogozik@microsoft.wsei.edu.pl>
2023-09-30 16:02:30 +0200

    first commit

---
+++ lab1/main.js
@@ -0,0 +1,20 @@
+    const one = document.getElementById("one");
+   const two = document.getElementById("two");
+   const three = document.getElementById("three");
+   const four = document.getElementById("four");
+   const mybutton = document.getElementById("submit");
+   
+   let sum, avg, min, max
+   
+   
+mybutton.addEventListener("click", () => {
+    const inputValue = one.value 
+    console.log(parseFloat(inputValue))
+});
+  
+  
+  
+  
+  
+  
+  
